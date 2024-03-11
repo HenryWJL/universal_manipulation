b2000207b9475ad4ffdb71b0df466587b0c9b92e
Junlin Wang <12112921@mail.sustech.edu.cn>
2024-03-11 19:02:09 +0800

    Create 00_process_videos_and_umi.py

---
+++ scripts_slam_pipeline/00_process_videos_and_umi.py
@@ -0,0 +1,174 @@
+import sys
+import os
+from pathlib import Path
+import numpy as np
+import click
+import shutil
+from exiftool import ExifToolHelper
+from umi.common.timecode_util import mp4_get_start_datetime
+
+
+@click.command(help="Transform raw IMU data to specified formats and move videos as well as processed IMU data to target directories.")
+@click.option("-l", "--load_dir", type=str, required=True, help="Directory where videos and raw IMU data are stored.")
+@click.option("-s", "--save_dir", type=str, default="", help="Directory used for saving videos and processed IMU data.")
+
+
+def main(load_dir):
+    load_dir = Path(os.path.expanduser(load_dir)).absolute()
+    input_dir = load_dir.joinpath('raw_videos')
+    output_dir = load_dir.joinpath('demos')
+
+    if not input_dir.is_dir():
+        input_dir.mkdir()
+
+        ### search for mp4 videos and move them to "raw_videos" directory
+        print("\"raw_videos\" does not exist! Create one and move all videos in.")
+        for mp4_path in list(load_dir.glob('VID*.mp4')):
+            out_path = input_dir.joinpath(mp4_path.name)
+            shutil.move(mp4_path, out_path)
+    
+    ### create mapping video if don't exist
+    mapping_vid_dir = input_dir.joinpath('mapping')
+    if (not mapping_vid_dir.is_dir()):
+        mapping_vid_dir.mkdir()
+
+        max_size = -1
+        max_path = None
+        for mp4_path in list(input_dir.glob('VID*.mp4')):
+            size = mp4_path.stat().st_size  # sizes of mp4 videos
+            # find mp4 video with maximum size and deem it as mapping video
+            if size > max_size:
+                max_size = size
+                max_path = mp4_path
+
+        print("Mapping video does not exist! Create one using the video with maximum size.")
+        out_path = mapping_vid_dir.joinpath(max_path.name)
+        shutil.move(max_path, out_path)
+        
+    ### create gripper calibration video if don't exist
+    gripper_cal_dir = input_dir.joinpath('gripper_calibration')
+    if not gripper_cal_dir.is_dir():
+        gripper_cal_dir.mkdir()
+        
+        serial_start_dict = dict()
+        serial_path_dict = dict()
+        with ExifToolHelper() as et:
+            for mp4_path in list(input_dir.glob('VID*.mp4')):
+                start_date = mp4_get_start_datetime(str(mp4_path))
+                meta = list(et.get_metadata(str(mp4_path)))[0]
+                cam_serial = meta['QuickTime:CameraSerialNumber']
+                # find mp4 video with earliest starting datatime and apply it for gripper calibration
+                if cam_serial in serial_start_dict:
+                    if start_date < serial_start_dict[cam_serial]:
+                        serial_start_dict[cam_serial] = start_date
+                        serial_path_dict[cam_serial] = mp4_path
+                else:
+                    serial_start_dict[cam_serial] = start_date
+                    serial_path_dict[cam_serial] = mp4_path
+        
+        for serial, path in serial_path_dict.items():
+            print(f"Gripper calibration videos do not exist. Selected {path.name} for camera serial {serial}")
+            out_path = gripper_cal_dir.joinpath(path.name)
+            shutil.move(path, out_path)
+
+    ### Move mp4 videos and processed IMU data to target directories in "demos"
+    input_mp4_paths = list(input_dir.glob('**/VID*.mp4'))
+    print(f'Found {len(input_mp4_paths)} mp4 videos')
+
+    with ExifToolHelper() as et:
+        for mp4_path in input_mp4_paths:
+            # symbolic links will be created later. If already exist, skip
+            if mp4_path.is_symlink():
+                print(f"Skipping {mp4_path.name}, already moved.")
+                continue
+
+            ### create directories used for storing metadata
+            """format (out_dname)
+
+            1. 'demo_<cam_serial>_year.month.day_hour.minute.second.xxxx'
+            2. 'mapping'
+            3. 'gripper_calibration_<cam_serial>_year.month.day_hour.minute.second.xxxx'
+
+            """
+            start_date = mp4_get_start_datetime(str(mp4_path))
+            meta = list(et.get_metadata(str(mp4_path)))[0]
+            cam_serial = meta['QuickTime:CameraSerialNumber']
+            out_dname = 'demo_' + cam_serial + '_' + start_date.strftime(r"%Y.%m.%d_%H.%M.%S.%f")
+            # strftime(): year.month.day_hour.minute.second.microsecond. e.g, 2024.03.01_10.26.20.849084
+
+            # special folders to rename
+            if mp4_path.parent.name.startswith('mapping'):
+                out_dname = "mapping"  # store mapping data
+
+            elif mp4_path.parent.name.startswith('gripper_cal'):  # store gripper calibration data
+                out_dname = "gripper_calibration_" + cam_serial + '_' + start_date.strftime(r"%Y.%m.%d_%H.%M.%S.%f")
+
+            this_out_dir = output_dir.joinpath(out_dname)  # <load_dir>/demos/<out_dname>
+            this_out_dir.mkdir(parents=True, exist_ok=True)
+            
+            ### move mp4 videos to target directories
+            vfname = 'raw_video.mp4'
+            out_video_path = this_out_dir.joinpath(vfname)  # <load_dir>/demos/<out_dname>/raw_video.mp4
+            shutil.move(mp4_path, out_video_path)
+
+            ### process and move IMU data to target directories
+            imu_dir = mp4_path.name[4: -4]
+            accel_path = list(load_dir.glob(f"{imu_dir}/*accel.csv"))[0]
+            gyro_path = list(load_dir.glob(f"{imu_dir}/*gyro.csv"))[0]
+
+            gyro_data = np.loadtxt(open(str(gyro_path), 'rb'), delimiter=",")
+            accel_data = np.loadtxt(open(str(accel_path), 'rb'), delimiter=",")
+            np.set_printoptions(suppress=True)  # TODO
+            imu_data = synchronize(gyro_data, accel_data)
+
+            ### create symbolic link for all videos. Links are in "raw_videos"
+            dots = os.path.join(*['..'] * len(mp4_path.parent.relative_to(load_dir).parts))
+            rel_path = str(out_video_path.relative_to(load_dir))
+            symlink_path = os.path.join(dots, rel_path)                
+            mp4_path.symlink_to(symlink_path)
+
+
+def synchronize(gyro_data, accel_data):
+    """
+    Synchronize gyro and accel data 
+
+    Returns:
+        gyro + accel + timestamp
+    """
+    idx_accel = 0
+    idx_gyro = 0
+    idx_imu = 0
+    num_gyro = gyro_data.shape[0]
+    num_accel = accel_data.shape[0]
+    imu_data = np.zeros((num_gyro, 7), dtype=np.float32)
+    while (accel_data[0, -1] > gyro_data[idx_gyro, -1]):
+        idx_gyro += 1
+
+    while (idx_accel + 1 < num_accel and idx_gyro < num_gyro):
+        # compute timestamp and acceleration difference
+        delta_time_accel = accel_data[(idx_accel + 1), -1] - accel_data[idx_accel, -1]
+        delta_accel = accel_data[(idx_accel + 1), 0: 3] - accel_data[idx_accel, 0: 3]
+        # combine imu data
+        while (idx_gyro < num_gyro and accel_data[idx_accel + 1, -1] >= gyro_data[idx_gyro, -1]):
+            imu_data[idx_imu, -1] = gyro_data[idx_gyro, -1]
+            # interpolate acceleration
+            imu_data[idx_imu, 3: 6] = accel_data[idx_accel, 0: 3] + \
+                (gyro_data[idx_gyro, -1] - accel_data[idx_accel, -1]) * delta_accel / delta_time_accel
+            # dump gyro data
+            imu_data[idx_imu, 0: 3] = gyro_data[idx_gyro, 0: 3]
+
+            idx_gyro += 1
+            idx_imu += 1
+
+        idx_accel += 1
+
+    imu_data = np.delete(imu_data, range(idx_imu, num_gyro), axis=0)
+
+    
+if __name__ == '__main__':
+    # if len(sys.argv) == 1:
+    #     main.main(['--help'])
+
+    # else:
+    #     main()
+    print(mp4_get_start_datetime("VID_20240307_184717.mp4").timestamp())
