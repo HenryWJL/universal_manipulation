7b2ec6dfd748d6d5f604cdba95eca3d73931bf84
Junlin Wang <12112921@mail.sustech.edu.cn>
2024-03-11 21:56:58 +0800

    Create 04_detect_aruco.py

---
+++ scripts_slam_pipeline/04_detect_aruco.py
@@ -0,0 +1,102 @@
+"""
+MIT License
+
+Copyright (c) 2023 Columbia Artificial Intelligence and Robotics Lab
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+"""
+"""
+Detect ArUco markers on the gripper
+"""
+import sys
+import os
+
+ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
+sys.path.append(ROOT_DIR)
+os.chdir(ROOT_DIR)
+
+import pathlib
+import click
+import multiprocessing
+import subprocess
+import concurrent.futures
+from tqdm import tqdm
+
+
+@click.command()
+@click.option('-i', '--input_dir', required=True, help='Directory for demos folder')
+@click.option('-ci', '--camera_intrinsics', required=True, help='Camera intrinsics json file (2.7k)')
+@click.option('-ac', '--aruco_yaml', required=True, help='Aruco config yaml file')
+@click.option('-n', '--num_workers', type=int, default=None)
+
+
+def main(input_dir, camera_intrinsics, aruco_yaml, num_workers):
+    input_dir = pathlib.Path(os.path.expanduser(input_dir))  # <session>/demos
+    input_video_dirs = [x.parent for x in input_dir.glob('*/raw_video.mp4')]
+    print(f'Found {len(input_video_dirs)} video dirs')
+    
+    assert os.path.isfile(camera_intrinsics)
+    assert os.path.isfile(aruco_yaml)
+
+    if num_workers is None:
+        num_workers = multiprocessing.cpu_count()
+
+    script_path = pathlib.Path(__file__).parent.parent.joinpath('scripts', 'detect_aruco.py')
+
+    with tqdm(total=len(input_video_dirs)) as pbar:
+        with concurrent.futures.ThreadPoolExecutor(max_workers=num_workers) as executor:
+            futures = set()
+            for video_dir in tqdm(input_video_dirs):
+                video_dir = video_dir.absolute()
+                video_path = video_dir.joinpath('raw_video.mp4')
+                pkl_path = video_dir.joinpath('tag_detection.pkl')
+                # if detection is already done, skip
+                if pkl_path.is_file():
+                    print(f"tag_detection.pkl already exists, skipping {video_dir.name}")
+                    continue
+
+                ### detect ArUco markers on the gripper
+                cmd = [
+                    'python', script_path,
+                    '--input', str(video_path),
+                    '--output', str(pkl_path),
+                    '--intrinsics_json', camera_intrinsics,
+                    '--aruco_yaml', aruco_yaml,
+                    '--num_workers', '1'
+                ]
+                # ignore
+                if len(futures) >= num_workers:
+                    completed, futures = concurrent.futures.wait(futures, 
+                        return_when=concurrent.futures.FIRST_COMPLETED)
+                    pbar.update(len(completed))
+                # ignore
+                futures.add(executor.submit(
+                    lambda x: subprocess.run(x, 
+                        capture_output=True), 
+                    cmd))
+
+            completed, futures = concurrent.futures.wait(futures)            
+            pbar.update(len(completed))
+
+    print("Done! Result:")
+    print([x.result() for x in completed])
+
+
+if __name__ == "__main__":
+    main()
