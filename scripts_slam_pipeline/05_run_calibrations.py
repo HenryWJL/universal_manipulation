4bd41a3f1e3ac9f35b83eafd3aa7e1ce44bf89bf
Junlin Wang <12112921@mail.sustech.edu.cn>
2024-03-18 12:19:04 +0800

    Update 05_run_calibrations.py

--- scripts_slam_pipeline/05_run_calibrations.py
+++ scripts_slam_pipeline/05_run_calibrations.py
@@ -26,19 +26,16 @@ SLAM tag calibration and gripper range calibration
 """
 import sys
 import os
+import pathlib
+import click
+import subprocess
 
 ROOT_DIR = os.path.dirname(os.path.dirname(__file__))
 sys.path.append(ROOT_DIR)
 os.chdir(ROOT_DIR)
 
-import pathlib
-import click
-import subprocess
-
-
 @click.command()
 @click.argument('session_dir', nargs=-1)
-
 
 def main(session_dir):
     script_dir = pathlib.Path(__file__).parent.parent.joinpath('scripts')
@@ -47,22 +44,18 @@ def main(session_dir):
         session = pathlib.Path(session)
         demos_dir = session.joinpath('demos')
         mapping_dir = demos_dir.joinpath('mapping')
-        
-        ### slam tag calibration
+        ### find relevant data
         script_path = script_dir.joinpath('calibrate_slam_tag.py')
         assert script_path.is_file()
 
         tag_path = mapping_dir.joinpath('tag_detection.pkl')
-        assert tag_path.is_file()
+        assert tag_path.is_file(), f"\"tag_detection.pkl\" not found in {str(mapping_dir)}!"
 
         csv_path = mapping_dir.joinpath('camera_trajectory.csv')
-        if not csv_path.is_file():
-            csv_path = mapping_dir.joinpath('mapping_camera_trajectory.csv')
-            print("camera_trajectory.csv not found! using mapping_camera_trajectory.csv")
-        assert csv_path.is_file()
+        assert csv_path.is_file(), f"\"camera_trajectory.csv\" not found in {str(mapping_dir)}!"
 
         slam_tag_path = mapping_dir.joinpath('tx_slam_tag.json')
-        
+        ### run slam tag calibration
         cmd = [
             'python', str(script_path),
             '--tag_detection', str(tag_path),
@@ -70,25 +63,32 @@ def main(session_dir):
             '--output', str(slam_tag_path),
             '--keyframe_only'
         ]
-        subprocess.run(cmd)
+        result = subprocess.run(cmd)
+
+        if result.returncode != 0:
+            print("SLAM tag calibration failed!")
+            exit(1)
         
         ### gripper range calibration
         script_path = script_dir.joinpath('calibrate_gripper_range.py')
         assert script_path.is_file()
         
         for gripper_dir in demos_dir.glob("gripper_calibration*"):
-            
             tag_path = gripper_dir.joinpath('tag_detection.pkl')
-            assert tag_path.is_file()
+            assert tag_path.is_file(), f"\"tag_detection.pkl\" not found in {str(gripper_dir)}!"
 
             gripper_range_path = gripper_dir.joinpath('gripper_range.json')
-
+            ### run gripper range calibration
             cmd = [
                 'python', str(script_path),
                 '--input', str(tag_path),
                 '--output', str(gripper_range_path)
             ]
-            subprocess.run(cmd)
+            result = subprocess.run(cmd)
+
+            if result.returncode != 0:
+                print(f"Gripper range calibration failed! Error in {str(gripper_dir)}.")
+                exit(1)
 
             
 if __name__ == "__main__":
