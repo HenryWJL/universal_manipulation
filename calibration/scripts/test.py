1865dc6387328bb200fcab7cdc45eb0d4c7062b2
HenryWJL <wangjunlin131@gmail.com>
2024-03-24 22:39:34 +0800

    delete test.py

--- calibration/scripts/test.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime
-from datetime import timedelta
-
-date = datetime(2024, 3, 24, 21, 3, 58, 300)
-date = date + timedelta(microseconds=30)
-ts = date.timestamp()
-a = "1234"
-print(float(a))
