efd9e1e6e668bd224bdb54136dd9012da15c7246
Junlin Wang <12112921@mail.sustech.edu.cn>
2024-04-08 16:24:35 +0800

    Update 05_run_calibrations.py

--- scripts_slam_pipeline/05_run_calibrations.py
+++ scripts_slam_pipeline/05_run_calibrations.py
@@ -38,57 +38,55 @@ os.chdir(ROOT_DIR)
 @click.argument('session_dir', nargs=-1)
 
 def main(session_dir):
-    script_dir = pathlib.Path(__file__).parent.parent.joinpath('scripts')
+    script_dir = pathlib.Path(ROOT_DIR).joinpath('scripts')
+    session_dir = pathlib.Path(session_dir)
+    demos_dir = session_dir.joinpath('demos')
+    mapping_dir = demos_dir.joinpath('mapping')
+    ### find relevant data
+    script_path = script_dir.joinpath('calibrate_slam_tag.py')
+    assert script_path.is_file()
+
+    tag_path = mapping_dir.joinpath('tag_detection.pkl')
+    assert tag_path.is_file(), f"\"tag_detection.pkl\" not found in {str(mapping_dir)}!"
+
+    csv_path = mapping_dir.joinpath('camera_trajectory.csv')
+    assert csv_path.is_file(), f"\"camera_trajectory.csv\" not found in {str(mapping_dir)}!"
+
+    slam_tag_path = mapping_dir.joinpath('tx_slam_tag.json')
+    ### run slam tag calibration
+    cmd = [
+        'python', str(script_path),
+        '--tag_detection', str(tag_path),
+        '--csv_trajectory', str(csv_path),
+        '--output', str(slam_tag_path),
+        '--keyframe_only'
+    ]
+    result = subprocess.run(cmd)
+
+    if result.returncode != 0:
+        print("SLAM tag calibration failed!")
+        exit(1)
     
-    for session in session_dir:
-        session = pathlib.Path(session)
-        demos_dir = session.joinpath('demos')
-        mapping_dir = demos_dir.joinpath('mapping')
-        ### find relevant data
-        script_path = script_dir.joinpath('calibrate_slam_tag.py')
-        assert script_path.is_file()
+    ### gripper range calibration
+    script_path = script_dir.joinpath('calibrate_gripper_range.py')
+    assert script_path.is_file()
+    
+    for gripper_dir in demos_dir.glob("gripper_calibration*"):
+        tag_path = gripper_dir.joinpath('tag_detection.pkl')
+        assert tag_path.is_file(), f"\"tag_detection.pkl\" not found in {str(gripper_dir)}!"
 
-        tag_path = mapping_dir.joinpath('tag_detection.pkl')
-        assert tag_path.is_file(), f"\"tag_detection.pkl\" not found in {str(mapping_dir)}!"
-
-        csv_path = mapping_dir.joinpath('camera_trajectory.csv')
-        assert csv_path.is_file(), f"\"camera_trajectory.csv\" not found in {str(mapping_dir)}!"
-
-        slam_tag_path = mapping_dir.joinpath('tx_slam_tag.json')
-        ### run slam tag calibration
+        gripper_range_path = gripper_dir.joinpath('gripper_range.json')
+        ### run gripper range calibration
         cmd = [
             'python', str(script_path),
-            '--tag_detection', str(tag_path),
-            '--csv_trajectory', str(csv_path),
-            '--output', str(slam_tag_path),
-            '--keyframe_only'
+            '--input', str(tag_path),
+            '--output', str(gripper_range_path)
         ]
         result = subprocess.run(cmd)
 
         if result.returncode != 0:
-            print("SLAM tag calibration failed!")
+            print(f"Gripper range calibration failed! Error in {str(gripper_dir)}.")
             exit(1)
-        
-        ### gripper range calibration
-        script_path = script_dir.joinpath('calibrate_gripper_range.py')
-        assert script_path.is_file()
-        
-        for gripper_dir in demos_dir.glob("gripper_calibration*"):
-            tag_path = gripper_dir.joinpath('tag_detection.pkl')
-            assert tag_path.is_file(), f"\"tag_detection.pkl\" not found in {str(gripper_dir)}!"
-
-            gripper_range_path = gripper_dir.joinpath('gripper_range.json')
-            ### run gripper range calibration
-            cmd = [
-                'python', str(script_path),
-                '--input', str(tag_path),
-                '--output', str(gripper_range_path)
-            ]
-            result = subprocess.run(cmd)
-
-            if result.returncode != 0:
-                print(f"Gripper range calibration failed! Error in {str(gripper_dir)}.")
-                exit(1)
 
             
 if __name__ == "__main__":
